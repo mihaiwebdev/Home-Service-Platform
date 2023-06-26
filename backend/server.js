0954599249780a0a501f20389d88d448599cbf87
mihaiwebdev <1mihaisava1@gmail.com>
2023-06-26 15:12:05 +0300

    sendFile fix

--- backend/server.js
+++ backend/server.js
@@ -64,8 +64,8 @@ app.use('/api/v1/contracts', contracts);
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist'), { dotfiles: 'allow' }));
-    app.get('*', (req, res) => res.sendFile(__dirname, 'frontend' , 'dist', 'index.html'));
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 
 } else {
     app.get('/', (req, res) => res.send('Server is ready'));
